00ad0a765cebfa9f0703ff28247ce82785bd73fa
Shanya Hushyar <95575258+Shanyahushyar@users.noreply.github.com>
2021-12-20 13:50:58 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Shanya Hushyar '),
+        ),
+      ),
+      body: Column(
+        children: [
+          const Text('slaw Kurdistan'),
+          Image.network(
+              'https://cdn.kurdistan24.net/resources/files/2017/04/Kurdistanflag35.jpg')
+        ],
+      ),
+    );
+  }
+}
